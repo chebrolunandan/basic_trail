c79dd0aa75420b26dfdea5de21396036e988c9a2
Nandan <2100090004@kluniversity.in>
2023-08-24 22:37:34 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello World</h1>
+        <p>This is just a demo to check whether program works or not. Main data representation present in App.js</p>
+    </div>
+  );
+}
+
+export default App;
